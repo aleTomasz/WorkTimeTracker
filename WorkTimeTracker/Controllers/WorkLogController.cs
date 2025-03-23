508d2c7bc4eb0c2d4ff11ba304d7538a3d52efe2
aleTomasz <wiatrtomasz@gmail.com>
2025-03-23 21:32:24 +0100

    Add edit and delete functionality for work logs

--- WorkTimeTracker/Controllers/WorkLogController.cs
+++ WorkTimeTracker/Controllers/WorkLogController.cs
@@ -53,6 +53,49 @@ namespace WorkTimeTracker.Controllers
             _context.WorkLogs.Add(log);
             _context.SaveChanges();
             return RedirectToAction("Index");
+
+        }
+
+
+        public IActionResult Edit(int id)
+        {
+            var log = _context.WorkLogs.Find(id);
+            if (log == null || log.EndTime == null)
+            {
+                return NotFound();
+            }
+
+            return View(log);
+        }
+
+
+        [HttpPost]
+        public IActionResult Edit(WorkLog updatedLog)
+        {
+            var existingLog = _context.WorkLogs.Find(updatedLog.Id);
+            if (existingLog == null)
+            {
+                return NotFound();
+            }
+
+            existingLog.StartTime = updatedLog.StartTime;
+            existingLog.EndTime = updatedLog.EndTime;
+
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+
+        [HttpPost]
+        public IActionResult Delete(int id)
+        {
+            var log = _context.WorkLogs.Find(id);
+            if (log != null)
+            {
+                _context.WorkLogs.Remove(log);
+                _context.SaveChanges();
+            }
+            return RedirectToAction("Index");
         }
 
         [HttpPost]
--- WorkTimeTracker/obj/Debug/net8.0/WorkTimeTracker.AssemblyInfo.cs
+++ WorkTimeTracker/obj/Debug/net8.0/WorkTimeTracker.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WorkTimeTracker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a4bc5e9f37a574560f95d824876d73a333a841f4")]
 [assembly: System.Reflection.AssemblyProductAttribute("WorkTimeTracker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WorkTimeTracker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
